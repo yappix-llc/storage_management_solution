ce5d8c3413750507ad8fb04a78b8f2f572b493f3
Renat Usmanov <yappix@icloud.com>
2024-11-08 22:42:17 +0300

    colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ const poppins = Poppins({
 })
 
 export const metadata: Metadata = {
-  title: "StoreIt",
-  description: "StoreIt - The only storage solution you need.",
+  title: "BridgeInto",
+  description: "BridgeInto - The only storage solution you need.",
 };
 
 export default function RootLayout({
